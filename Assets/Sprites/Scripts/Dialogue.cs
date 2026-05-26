ced6c55e9a5bf5195397ca40aa514d589d3dcac1
maferoldan09-arch <mafe.roldan.09@gmail.com>
2026-05-25 20:59:45 -0500

    Animación del player y inicio de panel de dialogo

---
+++ Assets/Sprites/Scripts/Dialogue.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+
+    [Header("Dialogue")]
+    [SerializeField, TextArea(4,6)] 
+    private string[] dialogueLines;
+
+    [SerializeField] private float typingTime = 0.05f;
+
+    private bool isPlayerInRange;
+    private bool didDialogueStart;
+    private int lineIndex;
+
+    private void Start()
+    {
+        // El panel inicia apagado
+        dialoguePanel.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // Si el jugador está cerca y presiona E
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            // Si el diálogo no ha empezado
+            if (!didDialogueStart)
+            {
+                StartDialogue();
+            }
+            // Si la línea ya terminó de escribirse
+            else if (dialogueText.text == dialogueLines[lineIndex])
+            {
+                NextDialogueLine();
+            }
+            // Completa instantáneamente la línea
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[lineIndex];
+            }
+        }
+    }
+
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+
+        dialoguePanel.SetActive(true);
+
+        lineIndex = 0;
+
+        StartCoroutine(ShowLine());
+    }
+
+    private void NextDialogueLine()
+    {
+        lineIndex++;
+
+        if (lineIndex < dialogueLines.Length)
+        {
+            StartCoroutine(ShowLine());
+        }
+        else
+        {
+            EndDialogue();
+        }
+    }
+
+    private void EndDialogue()
+    {
+        didDialogueStart = false;
+
+        dialoguePanel.SetActive(false);
+
+        dialogueText.text = "";
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        foreach (char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+
+            yield return new WaitForSeconds(typingTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+
+            Debug.Log("Jugador cerca del NPC");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+
+            EndDialogue();
+
+            Debug.Log("Jugador salió del NPC");
+        }
+    }
+}
