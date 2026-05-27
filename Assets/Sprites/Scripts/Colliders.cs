967a562ad1b092147705aab4b50b861c4fc5f18b
maferoldan09-arch <mafe.roldan.09@gmail.com>
2026-05-27 04:30:19 -0500

    Inicio coleccionables

--- Assets/Sprites/Scripts/Colliders.cs
+++ Assets/Sprites/Scripts/Colliders.cs
@@ -1,36 +1,12 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using TMPro;
 
-public class AguaColision : MonoBehaviour
+public class Colliders : MonoBehaviour
 {
-    public TextMeshProUGUI notificationText;
-
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
-            if(notificationText != null)
-            {
-                notificationText.text =
-                    "Caíste al agua";
-            }
-
-            Debug.Log("Game Over - Agua");
-
-            Invoke(
-                "ReiniciarEscena",
-                2f
-            );
+            other.GetComponent<PlayerController>().Morir();
         }
-    }
-
-    void ReiniciarEscena()
-    {
-        SceneManager.LoadScene(
-            SceneManager
-            .GetActiveScene()
-            .name
-        );
     }
 }
--- Assets/Sprites/Scripts/PlayerController.cs
+++ Assets/Sprites/Scripts/PlayerController.cs
@@ -1,26 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float velocidad = 5f;
 
-    public float fuerzaSalto = 10f; 
-    public float longitudRaycast = 0.1f; 
-    public LayerMask capaSuelo; 
+    public float fuerzaSalto = 10f;
+    public float longitudRaycast = 0.1f;
+    public LayerMask capaSuelo;
 
-    private bool enSuelo; 
+    private bool enSuelo;
     public bool muerto;
 
-    private Rigidbody2D rb; 
+    private Rigidbody2D rb;
 
     public Animator animator;
+
+    // PUNTAJE
+    public int score = 0;
+
+    // MONEDAS
+    public int monedas = 0;
+    public int totalMonedas = 0;
+
+    // BILLETES
+    public int billetes = 0;
+    public int totalBilletes = 0;
+
+    // COFRE
+    public bool hasCofre = false;
+
+    // UI
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI monedasText;
+    public TextMeshProUGUI billetesText;
+    public TextMeshProUGUI notificationText;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+
+        UpdateScore();
+        UpdateMonedas();
+        UpdateBilletes();
     }
 
     // Update is called once per frame
@@ -37,12 +62,16 @@ public class PlayerController : MonoBehaviour
             {
                 rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
             }
+
+            animator.SetBool("ensuelo", enSuelo);
         }
-        
-        animator.SetBool("ensuelo", enSuelo);
-        animator.SetBool("muerto", muerto);
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Morir();
+        }
     }
-       
+
     public void Movimiento()
     {
         float velocidadX = Input.GetAxis("Horizontal") * Time.deltaTime * velocidad;
@@ -62,6 +91,88 @@ public class PlayerController : MonoBehaviour
         Vector3 posicion = transform.position;
 
         transform.position = new Vector3(velocidadX + posicion.x, posicion.y, posicion.z);
+    }
+
+    public void Morir()
+    {
+        muerto = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // MONEDAS
+        if(other.CompareTag("Moneda"))
+        {
+            monedas++;
+            score += 1;
+
+            UpdateMonedas();
+            UpdateScore();
+
+            ShowNotification("Moneda recogida");
+
+            Destroy(other.gameObject);
+        }
+
+        // BILLETES
+        if(other.CompareTag("Billete"))
+        {
+            billetes++;
+            score += 5;
+
+            UpdateBilletes();
+            UpdateScore();
+
+            ShowNotification("Billete recogido");
+
+            Destroy(other.gameObject);
+        }
+
+        // COFRE FINAL
+        if(other.CompareTag("Cofre"))
+        {
+            hasCofre = true;
+
+            ShowNotification("¡Ganaste!");
+
+            Debug.Log("Ganaste");
+        }
+
+        // AGUA
+        if(other.CompareTag("Agua"))
+        {
+            Morir();
+
+            ShowNotification("Game Over");
+
+            Debug.Log("Game Over");
+        }
+    }
+
+    // UI
+
+    void UpdateScore()
+    {
+        if(scoreText != null)
+            scoreText.text = "Puntaje: " + score;
+    }
+
+    void UpdateMonedas()
+    {
+        if(monedasText != null)
+            monedasText.text = "Monedas: " + monedas;
+    }
+
+    void UpdateBilletes()
+    {
+        if(billetesText != null)
+            billetesText.text = "Billetes: " + billetes;
+    }
+
+    void ShowNotification(string message)
+    {
+        if(notificationText != null)
+            notificationText.text = message;
     }
 
     void OnDrawGizmos()
