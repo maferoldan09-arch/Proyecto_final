6862003b1271dfc085bb7c12ef2686f63dee199b
maferoldan09-arch <mafe.roldan.09@gmail.com>
2026-05-26 21:13:03 -0500

    Parallax

--- Assets/Sprites/Scripts/ParallaxMovement.cs
+++ Assets/Sprites/Scripts/ParallaxMovement.cs
@@ -56,7 +56,7 @@ public class ParallaxMovement : MonoBehaviour
     private void LateUpdate()
     {
         distance = cam.position.x - camStartPos.x;
-        transform.position = new Vector3(cam.position.x - 1, transform.position.y, 1.72f);
+        transform.position = new Vector3(cam.position.x - 1, transform.position.y, 3.8f);
 
         for (int i = 0; i < backgrounds.Length; i++)
         {
