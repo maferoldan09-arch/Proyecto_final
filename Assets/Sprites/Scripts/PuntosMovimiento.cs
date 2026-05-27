4466a0e08d1f09ac3bf235334996090ae56689df
maferoldan09-arch <mafe.roldan.09@gmail.com>
2026-05-26 23:29:07 -0500

    Continuación del tilemap y arreglo en la plataforma movil

--- Assets/Sprites/Scripts/PuntosMovimiento.cs
+++ Assets/Sprites/Scripts/PuntosMovimiento.cs
@@ -10,51 +10,65 @@ public class PlataformaMovil : MonoBehaviour
     private int siguientePlataforma = 1;
     private bool ordenPlataformas = true;
 
-    private void Update()
+    private Vector3 ultimaPosicion;
+    private Vector3 movimientoPlataforma;
+
+    private void Start()
     {
-        if (ordenPlataformas && siguientePlataforma + 1 >= puntosMovimiento.Length)
+        ultimaPosicion = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (ordenPlataformas &&
+            siguientePlataforma + 1 >= puntosMovimiento.Length)
         {
             ordenPlataformas = false;
         }
 
-        if (!ordenPlataformas && siguientePlataforma <= 0)
+        if (!ordenPlataformas &&
+            siguientePlataforma <= 0)
         {
             ordenPlataformas = true;
         }
 
-        if (Vector2.Distance(transform.position,
-            puntosMovimiento[siguientePlataforma].position) < 0.1f)
+        if (Vector2.Distance(
+            transform.position,
+            puntosMovimiento[siguientePlataforma].position)
+            < 0.1f)
         {
             if (ordenPlataformas)
-            {
                 siguientePlataforma++;
-            }
             else
-            {
                 siguientePlataforma--;
-            }
         }
+
+        ultimaPosicion = transform.position;
 
         transform.position = Vector2.MoveTowards(
             transform.position,
             puntosMovimiento[siguientePlataforma].position,
-            velocidadMovimiento * Time.deltaTime
+            velocidadMovimiento * Time.fixedDeltaTime
         );
+
+        movimientoPlataforma =
+            transform.position - ultimaPosicion;
     }
 
-    private void OnCollisionEnter2D(Collision2D other)
+    private void OnCollisionStay2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            other.transform.SetParent(transform);
-        }
-    }
+            Rigidbody2D rb =
+                other.gameObject.GetComponent<Rigidbody2D>();
 
-    private void OnCollisionExit2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            other.transform.SetParent(null);
+            if (rb != null)
+            {
+                rb.position += new Vector2(
+                    movimientoPlataforma.x,
+                    movimientoPlataforma.y
+                );
+            }
         }
     }
 }
