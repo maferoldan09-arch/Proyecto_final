51f65fd789c3300f33e5dd8d648af576a2bd8f0c
maferoldan09-arch <mafe.roldan.09@gmail.com>
2026-05-27 22:05:31 -0500

    Organización UI

--- Assets/Sprites/Scripts/UI/Buttons.cs
+++ Assets/Sprites/Scripts/UI/Buttons.cs
@@ -5,21 +5,38 @@ public class Buttons : MonoBehaviour
 {
     public GameObject panelCreditos;
     public GameObject panelConfiguracion;
+    public GameObject panelPausa;
+    public GameObject panelInstrucciones;
+
+    void Start()
+    {
+        Time.timeScale = 0f;
+        panelInstrucciones.SetActive(true);
+    }
+
+    // JUGAR
 
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
 
+    // MENU
+
     public void GoMainMenu()
     {
+        Time.timeScale = 1f;
         SceneManager.LoadScene("Mainmenu");
     }
+
+    // SALIR
 
     public void QuitGame()
     {
         Application.Quit();
     }
+
+    // CREDITOS
 
     public void AbrirCreditos()
     {
@@ -31,6 +48,8 @@ public class Buttons : MonoBehaviour
         panelCreditos.SetActive(false);
     }
 
+    // CONFIGURACION
+
     public void AbrirConfiguracion()
     {
         panelConfiguracion.SetActive(true);
@@ -39,5 +58,27 @@ public class Buttons : MonoBehaviour
     public void CerrarConfiguracion()
     {
         panelConfiguracion.SetActive(false);
+    }
+
+    // PAUSA
+
+    public void PausarJuego()
+    {
+        Time.timeScale = 0f;
+        panelPausa.SetActive(true);
+    }
+
+    public void ReanudarJuego()
+    {
+        Time.timeScale = 1f;
+        panelPausa.SetActive(false);
+    }
+
+    // INSTRUCCIONES
+
+    public void CerrarInstrucciones()
+    {
+        panelInstrucciones.SetActive(false);
+        Time.timeScale = 1f;
     }
 }
